5d78158cda85de2d7c11a86ea0563853e2c44aa2
Pranay Goud <131458600+pranaygoud01@users.noreply.github.com>
2025-05-09 07:30:56 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,23 +47,32 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full h-[100vh] flex justify-center items-center bg-white">
+    <div className="w-full h-[100vh] flex justify-center overflow-hidden items-center bg-white">
       {showBranchPopup ? (
-        <div className="w-10/12 h-fit max-lg:w-11/12 max-lg:px-8 p-10 bg-white shadow-xl rounded-xl flex flex-col gap-6">
+        <div className="w-full h-fit max-lg:w-11/12 max-lg:px-8 p-10 bg-white shadow-xl rounded-xl flex flex-col gap-6">
           <h1 className="font-semibold text-xl text-center">Select Your Branch</h1>
-          <div className="grid grid-cols-2 gap-4">
-            {branches.map((branch) => (
+          <div className="grid grid-cols-2 max-lg:grid-cols-1 gap-4">
+            
               <button
-                key={branch}
-                onClick={() => handleBranchSelect(branch)}
-                className="h-92 max-lg:h-30 hover:shadow-md w-full bg-cover cursor-pointer bg-no-repeat bg-center  font-semibold text-white text-sm rounded-lg "
+                key='Harlow'
+                onClick={() => handleBranchSelect('Harlow')}
+                className="h-[90vh] max-lg:h-[38vh] hover:shadow-md w-full bg-cover cursor-pointer bg-no-repeat bg-center  font-semibold text-white text-sm rounded-lg "
                style={{backgroundImage:`url('https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`}}
               >
-                <div className='w-full flex items-center max-lg:text-xl text-white justify-center text-3xl h-full rounded-lg bg-black/40'>
-                  {branch}
+                <div className='w-full flex items-center font-bold max-lg:text-xl text-white justify-center text-5xl h-full rounded-lg bg-black/40'>
+                  Old Harlow
                 </div>
               </button>
-            ))}
+             <button
+                key="Bishop"
+                onClick={() => handleBranchSelect('Bishop')}
+                className="h-[90vh] max-lg:h-[38vh] hover:shadow-md w-full bg-cover cursor-pointer bg-no-repeat bg-center  font-semibold text-white text-sm rounded-lg "
+               style={{backgroundImage:`url('https://images.unsplash.com/photo-1493770348161-369560ae357d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`}}
+              >
+                <div className='w-full flex items-center font-bold max-lg:text-xl text-white justify-center text-5xl h-full rounded-lg bg-black/40'>
+                  Bishop's Stortford
+                </div>
+              </button>
           </div>
         </div>
       ) : (
